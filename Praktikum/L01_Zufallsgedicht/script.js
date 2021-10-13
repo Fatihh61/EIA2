581686417a84f2f65e4eb4fb44d04f5bd94b563b
Fatihh61 <temiz-fatih@hotmail.com>
2021-10-13 16:51:47 +0200

    L01_gecleaned

--- Praktikum/L01_Zufallsgedicht/script.js
+++ Praktikum/L01_Zufallsgedicht/script.js
@@ -3,45 +3,18 @@ var Aufgabe01;
     var subjekte = ["Harry", "Hermine", "Ron", "Hagrid", "Dumbledore"];
     var prädikate = ["braut", "liebt", "studiert", "zaubert", "avada kedavra auf"];
     var objekte = ["Dementoren", "Zaubertränke", "Trolle", "Lupin", "Hogwarts"];
-    // Gibt die Arrays mit den Reimen wieder
-    console.log(subjekte);
-    console.log(prädikate);
-    console.log(objekte);
     for (var i = 5; i >= 1; i--) {
-        /*
-        // Gibt die zufälligen Reime aus jeweils einem Array wieder
-        console.log(getVerse([subjekte[Math.floor(Math.random() * subjekte.length)]], [prädikate[Math.floor(Math.random() * prädikate.length)]], [objekte[Math.floor(Math.random() * objekte.length)]], ""));
-        */
         console.log(getVerse(subjekte, prädikate, objekte));
     }
     function getVerse(_subjekte, _prädikate, _objekte) {
         var vers = [];
-        var test1 = (subjekte[Math.floor(Math.random() * subjekte.length)]);
-        var test2 = (prädikate[Math.floor(Math.random() * objekte.length)]);
-        var test3 = (objekte[Math.floor(Math.random() * prädikate.length)]);
-        // Trennen des einzelnen Elements aus dem jeweiligen Array
-        /*
-        objekte.splice(test2);
-        prädikate.splice(test3);
-        */
-        // pushen des Elements in die Variable vers
-        vers.push(test1);
-        vers.push(test2);
-        vers.push(test3);
-        /*
-        vers.push(objekte);
-        prädikate.push(vers);
-        */
-        /*
-        console.log(subjekte);
-        console.log(objekte);
-        console.log(prädikate);
-        */
+        var zufallszahl01 = (subjekte[Math.floor(Math.random() * subjekte.length)]);
+        var zufallszahl02 = (prädikate[Math.floor(Math.random() * objekte.length)]);
+        var zufallszahl03 = (objekte[Math.floor(Math.random() * prädikate.length)]);
+        vers.push(zufallszahl01);
+        vers.push(zufallszahl02);
+        vers.push(zufallszahl03);
         console.log(vers);
-        /*
-        console.log(test2);
-        console.log(test3);
-        */
     }
 })(Aufgabe01 || (Aufgabe01 = {}));
 //# sourceMappingURL=script.js.map
--- Praktikum/L01_Zufallsgedicht/script.ts
+++ Praktikum/L01_Zufallsgedicht/script.ts
@@ -1,67 +1,26 @@
 namespace Aufgabe01 {
 
 
-
 let subjekte: string [] = ["Harry", "Hermine", "Ron", "Hagrid", "Dumbledore"];
 let prädikate: string [] = ["braut", "liebt", "studiert", "zaubert", "avada kedavra auf"];
 let objekte: string [] = ["Dementoren", "Zaubertränke", "Trolle", "Lupin", "Hogwarts"];
 
-// Gibt die Arrays mit den Reimen wieder
-console.log(subjekte);
-console.log(prädikate);
-console.log(objekte);
-
-
-
 for (let i: number = 5; i >= 1; i--) {
-    
-    /*
-    // Gibt die zufälligen Reime aus jeweils einem Array wieder
-    console.log(getVerse([subjekte[Math.floor(Math.random() * subjekte.length)]], [prädikate[Math.floor(Math.random() * prädikate.length)]], [objekte[Math.floor(Math.random() * objekte.length)]], ""));
-    */
+
     console.log(getVerse(subjekte, prädikate, objekte));
 }
 
-
 function getVerse(_subjekte: string[], _prädikate: string[], _objekte: string[]): void {
 
-    
-
     let vers: string [] = [];
+    let zufallszahl01: string = (subjekte[Math.floor (Math.random() * subjekte.length)]);
+    let zufallszahl02: string = (prädikate[Math.floor (Math.random() * objekte.length)]);
+    let zufallszahl03: string = (objekte[Math.floor( Math.random() * prädikate.length)]);
 
-    let test1: string = (subjekte[Math.floor (Math.random() * subjekte.length)]);
-    let test2: string = (prädikate[Math.floor (Math.random() * objekte.length)]);
-    let test3: string = (objekte[Math.floor( Math.random() * prädikate.length)]);
-
-    
-    // Trennen des einzelnen Elements aus dem jeweiligen Array
-    /*
-    objekte.splice(test2);
-    prädikate.splice(test3);
-    */
-   
-    // pushen des Elements in die Variable vers
-    vers.push(test1);
-    vers.push(test2);
-    vers.push(test3);
-
-    /*
-    vers.push(objekte);
-    prädikate.push(vers);
-    */
-
-    /*
-    console.log(subjekte);
-    console.log(objekte);
-    console.log(prädikate);
-    */
-    
+    vers.push(zufallszahl01);
+    vers.push(zufallszahl02);
+    vers.push(zufallszahl03);
 
     console.log(vers);
-    
-    /*
-    console.log(test2);
-    console.log(test3);
-    */
 }
 }
