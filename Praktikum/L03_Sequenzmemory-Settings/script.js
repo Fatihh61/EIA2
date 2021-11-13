1885d9c69d8b43daf204a3590bd2d903a657b37e
Fatihh61 <temiz-fatih@hotmail.com>
2021-11-13 15:05:31 +0100

    L03_Sequenzmemory-Setting_Update

---
+++ Praktikum/L03_Sequenzmemory-Settings/script.js
@@ -0,0 +1,184 @@
+"use strict";
+var L03_Sequenzmemory_Neu;
+(function (L03_Sequenzmemory_Neu) {
+    //Formular für die Karteneinstellungen
+    let form;
+    let gamefield;
+    //Start Button um das Spiel zu starten
+    let startButton;
+    //Div Element für die span Elemente
+    let container;
+    //Timer der runterzählt
+    let countdown;
+    //Array wo die Sequenzeingabe reingeschossen wird
+    let sequenceArray;
+    //Array wo die Sequenzeingabe reingeschossen wird
+    let sequenceArrayOriginal;
+    //Sequenzeingabe
+    let sequenzeingabe;
+    //FormData für die Auswertung des Formulars
+    let formData;
+    //Countdown
+    let timer;
+    //Karten 
+    let span;
+    //Eingegebene Sequenz in Buchstaben
+    let buchstabe;
+    let überprüfZahl;
+    let event;
+    let information;
+    let allInputElements;
+    let allSelectElements;
+    let backOfCard2;
+    let background2;
+    let fontColor2;
+    let sizeOfCard2;
+    let font2;
+    let schwierigkeitsgrad;
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        form = document.querySelector("form");
+        startButton = document.querySelector("button");
+        startButton.addEventListener("click", loadMemoryField);
+        console.log(_event);
+    }
+    function loadMemoryField(_event) {
+        container = document.getElementById("container_for_span");
+        countdown = document.getElementById("countdown");
+        form.classList.add("isHidden");
+        container.classList.remove("isHidden");
+        countdown.classList.remove("isHidden");
+        console.log(_event);
+        createCards();
+    }
+    function createCards() {
+        //Input von Sequence wird genommen
+        sequenzeingabe = document.getElementById("input");
+        console.log(sequenzeingabe);
+        //Eingegebene Sequenz wird in Buchstaben gesplitted und in ein Array geschossen
+        sequenceArray = sequenzeingabe.value.split("");
+        sequenceArrayOriginal = sequenzeingabe.value.split("");
+        console.log(sequenceArray);
+        formData = new FormData(document.forms[0]);
+        console.log(formData);
+        allInputElements = document.querySelectorAll("input");
+        allSelectElements = document.querySelectorAll("select");
+        for (let i = sequenceArray.length - 1; i >= 0; i--) {
+            //Eingabe im Array wird nun geshuffled und das Array wird gewürfelt
+            let randomNumber;
+            let shuffledCards;
+            //debugger;
+            randomNumber = Math.floor(Math.random() * (i + 1));
+            shuffledCards = sequenceArray[i];
+            sequenceArray[i] = sequenceArray[randomNumber];
+            sequenceArray[randomNumber] = shuffledCards;
+            //Karten werden nun als span Element erstellt und an jeden span ein zufälliger Buchstabe angeheftet
+            span = document.createElement("span");
+            buchstabe = document.createElement("p");
+            //debugger;
+            span.appendChild(buchstabe);
+            span.classList.add("card");
+            buchstabe.innerHTML = (sequenceArray[i]);
+            container.appendChild(span);
+            //buchstabe.classList.add("isHidden");
+            buchstabe.classList.add("buchstabe");
+            //Grö0e der Karte wird übergeben
+            sizeOfCard2 = allInputElements[0].value;
+            span.style.width = sizeOfCard2 + "px";
+            span.style.height = sizeOfCard2 + "px";
+            //Hintergrundfarbe des Spielfelds wird übergeben
+            background2 = allInputElements[1].value;
+            gamefield = document.getElementById("container_for_span");
+            gamefield.style.backgroundColor = background2;
+            //Farbe für Kartenrücken wird übergen
+            backOfCard2 = allInputElements[2].value;
+            span.style.backgroundColor = backOfCard2;
+            //Schriftfarbe wird übergeben
+            fontColor2 = allInputElements[3].value;
+            buchstabe.style.color = fontColor2;
+            //Schriftfamilie wird übergeben
+            font2 = allSelectElements[0].value;
+            buchstabe.style.fontFamily = font2;
+            /*span.addEventListener("click", function (): void {
+
+                buchstabe.classList.remove("isHidden");
+
+
+            });
+            */
+        }
+        setTimeout(function () { buchstabe.classList.add("isHidden"); }, 3000);
+        let pElement = document.querySelectorAll("p");
+        console.log(pElement);
+        span.addEventListener("click", checkCards);
+        startGame();
+    }
+    function checkCards(_event) {
+        let array = [];
+        let target = _event.target;
+        console.log(target);
+        let textContent = target.textContent;
+        console.log(textContent);
+        array.push(textContent);
+        buchstabe.classList.remove("isHidden");
+        /*for (let i: number = 0; i > 10; i++) {
+
+            if (sequenceArrayOriginal[i] == array[i]) {
+
+                console.log("Well done");
+
+
+            } else {
+                alert("Game Over");
+
+            }
+
+        }*/
+        if (sequenceArrayOriginal[0] == array[0]) {
+            console.log("Richtige Reihenfolge, mach weiter!");
+        }
+        else if (sequenceArrayOriginal[1] == array[1]) {
+            console.log("Richtige Reihenfolge, mach weiter!");
+        }
+        else if (sequenceArrayOriginal[2] == array[2]) {
+            console.log("Richtige Reihenfolge, mach weiter!");
+        }
+        else if (sequenceArrayOriginal[3] == array[3]) {
+            console.log("Richtige Reihenfolge, mach weiter!");
+        }
+        else if (sequenceArrayOriginal[4] == array[4]) {
+            console.log("Richtige Reihenfolge, mach weiter!");
+        }
+        else if (sequenceArrayOriginal[4] == array[4]) {
+            console.log("Richtige Reihenfolge, mach weiter!");
+        }
+        else if (sequenceArrayOriginal[5] == array[5]) {
+            console.log("Richtige Reihenfolge, mach weiter!");
+            console.log(sequenceArrayOriginal);
+            console.log(array);
+        }
+        else if (sequenceArrayOriginal[6] == array[6]) {
+            console.log("Richtige Reihenfolge, mach weiter!");
+        }
+        else {
+            alert("Game over");
+        }
+    }
+    function startGame() {
+        schwierigkeitsgrad = parseFloat(allSelectElements[1].value);
+        timer = document.getElementById("countdown");
+        let counting = setInterval(myCountdown, 1000);
+        function myCountdown() {
+            timer.innerHTML = "Remaining Time: " + schwierigkeitsgrad;
+            schwierigkeitsgrad--;
+            if (schwierigkeitsgrad == 0) {
+                stopTimer();
+            }
+        }
+        function stopTimer() {
+            console.log("Ende");
+            clearInterval(counting);
+        }
+    }
+})(L03_Sequenzmemory_Neu || (L03_Sequenzmemory_Neu = {}));
+//# sourceMappingURL=script.js.map
--- Praktikum/L03_Sequenzmemory-Settings/script.ts
+++ Praktikum/L03_Sequenzmemory-Settings/script.ts
@@ -42,17 +42,21 @@ namespace L03_Sequenzmemory_Neu {
 
     let information: string;
 
-    let backOfCard: FormDataEntryValue | null;
+    let allInputElements: NodeListOf<HTMLInputElement>;
 
-    let background: FormDataEntryValue | null;
+    let allSelectElements: NodeListOf<HTMLSelectElement>;
 
-    let fontColor: FormDataEntryValue | null;
+    let backOfCard2: string;
 
-    let sizeOfCard: FormDataEntryValue | null;
+    let background2: string;
 
-    let font: FormDataEntryValue | null;
+    let fontColor2: string;
 
-    let schwierigkeitsgrad: FormDataEntryValue | null;
+    let sizeOfCard2: string;
+
+    let font2: string;
+
+    let schwierigkeitsgrad: number;
 
 
 
@@ -99,6 +103,12 @@ namespace L03_Sequenzmemory_Neu {
         sequenceArrayOriginal = sequenzeingabe.value.split("");
         console.log(sequenceArray);
 
+        formData = new FormData(document.forms[0]);
+        console.log(formData);
+
+        allInputElements = document.querySelectorAll("input");
+        allSelectElements = document.querySelectorAll("select");
+
         for (let i: number = sequenceArray.length - 1; i >= 0; i--) {
 
             //Eingabe im Array wird nun geshuffled und das Array wird gewürfelt
@@ -120,50 +130,120 @@ namespace L03_Sequenzmemory_Neu {
             span.classList.add("card");
             buchstabe.innerHTML = (sequenceArray[i]);
             container.appendChild(span);
-            buchstabe.classList.add("isHidden");
+            //buchstabe.classList.add("isHidden");
             buchstabe.classList.add("buchstabe");
-            span.addEventListener("click", checkCards);
+
+            //Grö0e der Karte wird übergeben
+            sizeOfCard2 = allInputElements[0].value;
+            span.style.width = sizeOfCard2 + "px";
+            span.style.height = sizeOfCard2 + "px";
+
+            //Hintergrundfarbe des Spielfelds wird übergeben
+            background2 = allInputElements[1].value;
+            gamefield = <HTMLDivElement>document.getElementById("container_for_span");
+            gamefield.style.backgroundColor = background2;
+
+            //Farbe für Kartenrücken wird übergen
+            backOfCard2 = allInputElements[2].value;
+            span.style.backgroundColor = backOfCard2;
+
+            //Schriftfarbe wird übergeben
+            fontColor2 = allInputElements[3].value;
+            buchstabe.style.color = fontColor2;
+
+            //Schriftfamilie wird übergeben
+            font2 = allSelectElements[0].value;
+            buchstabe.style.fontFamily = font2;
+
+
+            /*span.addEventListener("click", function (): void {
+
+                buchstabe.classList.remove("isHidden");
+
+
+            });
+            */
         }
 
+        setTimeout(function (): void { buchstabe.classList.add("isHidden"); }, 3000);
 
-    
+
+        let pElement: NodeListOf<HTMLElement> = document.querySelectorAll("p");
+
+        console.log(pElement);
+
+
+        span.addEventListener("click", checkCards);
+
+
+
+
+
+
+
+
+
         startGame();
 
-        formData = new FormData(document.forms[0]);
-        console.log(formData);
-
-        backOfCard = formData.get("back_of_card");
-        span.style.backgroundColor = backOfCard.toString();
-
-        background = formData.get("background");
-        gamefield = <HTMLDivElement> document.getElementById("container_for_span");
-        gamefield.style.backgroundColor = background.toString();
-
-        fontColor = formData.get("fontColor");
-        buchstabe.style.color = fontColor.toString();
-
-        sizeOfCard = formData.get("cardsize");
-        span.style.width = sizeOfCard + "px";
-        span.style.height = sizeOfCard + "px";
-
-        font = formData.get("font");
-        buchstabe.style.fontFamily = font.toString();
-        console.log(font);
-
-        schwierigkeitsgrad = formData.get("schwierigkeitsgrad");
 
     }
 
 
     function checkCards(_event: MouseEvent): void {
 
+
+        let array: string[] = [];
+
         let target: HTMLElement = <HTMLElement>_event.target;
         console.log(target);
 
-        if (target.classList.contains("card")) {
-            buchstabe.classList.remove("isHidden");
+        let textContent: string = <string>target.textContent;
+        console.log(textContent);
+        array.push(textContent);
+
+
+
+        buchstabe.classList.remove("isHidden");
+
+        /*for (let i: number = 0; i > 10; i++) {
+
+            if (sequenceArrayOriginal[i] == array[i]) {
+
+                console.log("Well done");
+
+
+            } else {
+                alert("Game Over");
+
+            }
+
+        }*/
+
+        if (sequenceArrayOriginal[0] == array[0]) {
+            console.log("Richtige Reihenfolge, mach weiter!");
+        } else if (sequenceArrayOriginal[1] == array[1]) {
+            console.log("Richtige Reihenfolge, mach weiter!");
+        } else if (sequenceArrayOriginal[2] == array[2]) {
+            console.log("Richtige Reihenfolge, mach weiter!");
+        } else if (sequenceArrayOriginal[3] == array[3]) {
+            console.log("Richtige Reihenfolge, mach weiter!");
+        } else if (sequenceArrayOriginal[4] == array[4]) {
+            console.log("Richtige Reihenfolge, mach weiter!");
+        } else if (sequenceArrayOriginal[4] == array[4]) {
+            console.log("Richtige Reihenfolge, mach weiter!");
+        } else if (sequenceArrayOriginal[5] == array[5]) {
+            console.log("Richtige Reihenfolge, mach weiter!");
+            console.log(sequenceArrayOriginal);
+            console.log(array);
+        } else if (sequenceArrayOriginal[6] == array[6]) {
+            console.log("Richtige Reihenfolge, mach weiter!");
+        } else {
+            alert("Game over");
         }
     }
+
+
+
 
 
 
@@ -171,29 +251,22 @@ namespace L03_Sequenzmemory_Neu {
 
     function startGame(): void {
 
-        
+        schwierigkeitsgrad = parseFloat(allSelectElements[1].value);
 
         timer = <HTMLElement>document.getElementById("countdown");
 
-        //let time: HTMLElement = <HTMLElement> document.getElementById("time");
-
         let counting: number = setInterval(myCountdown, 1000);
-
-        let counter: number = 120;
 
 
         function myCountdown(): void {
 
-            timer.innerHTML = "Remaining Time: " + counter;
+            timer.innerHTML = "Remaining Time: " + schwierigkeitsgrad;
 
-            //console.log(counter);
-            counter--;
+            schwierigkeitsgrad--;
 
-            if (counter == 0) {
+            if (schwierigkeitsgrad == 0) {
                 stopTimer();
             }
-
-
         }
 
 
@@ -203,6 +276,4 @@ namespace L03_Sequenzmemory_Neu {
             clearInterval(counting);
         }
     }
-
-
 }
