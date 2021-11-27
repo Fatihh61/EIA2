86cbf29ac0dcf67998d08b1f06ee7ee2b5f9acee
Fatihh61 <temiz-fatih@hotmail.com>
2021-11-27 16:08:53 +0100

    L08.2_GoldenerHerbst_Update

--- Praktikum/L08.2_GoldenerHerbst/script.js
+++ Praktikum/L08.2_GoldenerHerbst/script.js
@@ -12,10 +12,12 @@ var L08_GoldenerHerbst;
         let horizone = crc2.canvas.height * golden;
         //Startposition für den ersten Berg
         let startPointofFirstMountainX = Math.random() * (-50) - 80;
+        let randomFirstTreeX = Math.random() * 100;
         drawBackGround();
         drawCloud();
         drawMountains(startPointofFirstMountainX, horizone);
         //drawTree(0, horizone - 50);
+        drawTree2(randomFirstTreeX, horizone + 50);
         //drawField(0, horizone + 150);
     }
     function drawBackGround() {
@@ -40,6 +42,8 @@ var L08_GoldenerHerbst;
             let numbY = Math.random() * 100 + 50;
             let translateX = Math.random() * (-4.5) + (-3.5);
             let translateY = Math.random() * (-0.2);
+            console.log(translateX);
+            console.log(translateY);
             clouds.arc(numbX, numbY, 15, 0, 2 * Math.PI);
             crc2.beginPath();
             crc2.fillStyle = gradient;
@@ -52,37 +56,29 @@ var L08_GoldenerHerbst;
     }
     function drawMountains(_startPointFirstMountainX, _horizoneY) {
         //Gradient für die Berge
-        let gradient = crc2.createLinearGradient(0, 0, 0, _horizoneY);
-        gradient.addColorStop(0, "lightgrey");
-        gradient.addColorStop(1, "black");
+        let gradient = crc2.createLinearGradient(0, 150, 0, _horizoneY);
+        gradient.addColorStop(1, "grey");
+        gradient.addColorStop(0, "white");
         let x = 0;
-        debugger;
+        let drawLineRandomX = 0;
+        //debugger;
         crc2.save();
         //Beginn der Zeichnung für die Berge
-        crc2.beginPath();
-        crc2.moveTo(_startPointFirstMountainX, _horizoneY);
         for (let i = 8; i > 0; i--) {
             //Zufällige Zahl für Zeichnung in X und Y Richtung
-            let drawLineRandomX = Math.random() * crc2.canvas.width;
-            let drawLineRandomY = Math.random() * 50 + 150;
+            drawLineRandomX = Math.random() * 50 + 100;
+            let drawLineRandomY = Math.random() * 80 + 50;
             x += drawLineRandomX;
-            /*do {
-                x += stepMin + Math.random() * (stepMax - stepMin);
-                let y: number = - _min - Math.random() * (_max - _min);
-    
-                crc2.lineTo(x, y);
-    
-            } while (x < crc2.canvas.width);
-            */
             console.log(drawLineRandomX);
             console.log(drawLineRandomY);
+            crc2.beginPath();
+            crc2.moveTo(_startPointFirstMountainX, _horizoneY);
             crc2.strokeStyle = "grey";
-            crc2.lineWidth = 5;
-            crc2.lineTo(drawLineRandomX, drawLineRandomY);
-            crc2.lineTo(drawLineRandomX + 150, _horizoneY);
+            crc2.lineWidth = 2;
+            crc2.lineTo(x - 50, drawLineRandomY);
+            crc2.lineTo(x + 150, _horizoneY);
             crc2.fillStyle = gradient;
             crc2.fill();
-            crc2.stroke();
         }
         crc2.closePath();
         crc2.restore();
@@ -94,13 +90,13 @@ var L08_GoldenerHerbst;
         crc2.restore();
         crc2.save();
         crc2.beginPath();
-        crc2.fillStyle = "green";
-        crc2.strokeStyle = "black";
+        crc2.strokeStyle = "green";
         crc2.moveTo(_startPositionX + 100, _startPositionY);
         crc2.lineTo(-10, 0);
         crc2.lineTo(10, 20);
         crc2.lineTo(5, 20);
-        crc2.fill();
+        crc2.lineWidth = 2;
+        crc2.stroke();
         crc2.closePath();
         crc2.restore();
     }
@@ -109,6 +105,79 @@ var L08_GoldenerHerbst;
         crc2.fillStyle = "green";
         crc2.fillRect(_startPositionX, _startPositionY, 800, 78);
         crc2.restore();
+    }
+    function drawTree2(_startPositionX, _startPositionY) {
+        let treeTrunkX = 25;
+        let treeTrunkY = 50;
+        let treeBushX = 12.5;
+        let treeBushX2 = 25;
+        let treeBushX3 = 37.5;
+        let treeBushX4 = 50;
+        let treeBushY = 50;
+        let treeBushY2 = 75;
+        let xAdd = 0;
+        let drawRandomTreeOnX;
+        crc2.save;
+        /*
+        crc2.beginPath();
+        crc2.moveTo(_startPositionX, _startPositionY);
+        console.log(_startPositionX, _startPositionY);
+        crc2.lineTo(_startPositionX + treeTrunkX, _startPositionY);
+        crc2.lineTo(_startPositionX + treeTrunkX, _startPositionY - treeTrunkY);
+        crc2.lineTo(_startPositionX, _startPositionY - treeTrunkY);
+        crc2.lineTo(_startPositionX, _startPositionY);
+        crc2.stroke();
+        crc2.fillStyle = "brown";
+        crc2.fill();
+        crc2.closePath();
+
+
+
+        crc2.beginPath();
+        crc2.moveTo(_startPositionX, _startPositionY - treeTrunkY);
+        crc2.lineTo(_startPositionX - treeBushX2, _startPositionY - treeBushY);
+        crc2.lineTo(_startPositionX - treeBushX2 + treeBushX3, _startPositionY - treeBushY - treeBushY2);
+        crc2.lineTo(_startPositionX + treeBushX4, _startPositionY - treeBushY);
+        crc2.lineTo(_startPositionX + treeBushX4 - treeBushX4, _startPositionY - treeBushY);
+        crc2.fillStyle = "darkgreen";
+        crc2.fill();
+        crc2.stroke();
+        */
+        for (let i = 0; i < 5; i++) {
+            drawRandomTreeOnX = Math.random() * 50 + 70;
+            xAdd += drawRandomTreeOnX;
+            console.log(drawRandomTreeOnX);
+            console.log(xAdd);
+            crc2.beginPath();
+            crc2.moveTo(_startPositionX, _startPositionY);
+            console.log(_startPositionX, _startPositionY);
+            crc2.lineTo(_startPositionX + treeTrunkX, _startPositionY);
+            crc2.lineTo(_startPositionX + treeTrunkX, _startPositionY - treeTrunkY);
+            crc2.lineTo(_startPositionX, _startPositionY - treeTrunkY);
+            crc2.lineTo(_startPositionX, _startPositionY);
+            crc2.stroke();
+            crc2.fillStyle = "brown";
+            crc2.fill();
+            crc2.closePath();
+            crc2.beginPath();
+            crc2.moveTo(_startPositionX, _startPositionY - treeTrunkY);
+            crc2.lineTo(_startPositionX - treeBushX2, _startPositionY - treeBushY);
+            crc2.lineTo(_startPositionX - treeBushX2 + treeBushX3, _startPositionY - treeBushY - treeBushY2);
+            crc2.lineTo(_startPositionX + treeBushX4, _startPositionY - treeBushY);
+            crc2.lineTo(_startPositionX + treeBushX4 - treeBushX4, _startPositionY - treeBushY);
+            crc2.fillStyle = "darkgreen";
+            crc2.fill();
+            crc2.stroke();
+        }
+        /*
+        crc2.lineTo(_startPositionX + treeTrunkX, _startPositionY - 50);
+        crc2.lineTo(_startPositionX + treeTrunkX + treeTrunkX, _startPositionY - 50);
+        crc2.lineTo(_startPositionX + treeTrunkX - treeBushX , _startPositionY - 50 - treeBushY);
+        //Hälfte des Baumes fertig
+        crc2.lineTo(_startPositionX + treeTrunkX - treeBushX2 , _startPositionY - 50);
+        crc2.stroke();
+        */
+        crc2.restore;
     }
 })(L08_GoldenerHerbst || (L08_GoldenerHerbst = {}));
 //# sourceMappingURL=script.js.map
--- Praktikum/L08.2_GoldenerHerbst/script.ts
+++ Praktikum/L08.2_GoldenerHerbst/script.ts
@@ -18,6 +18,8 @@ namespace L08_GoldenerHerbst {
         //Startposition für den ersten Berg
         let startPointofFirstMountainX: number = Math.random() * (-50) - 80;
 
+        let randomFirstTreeX: number = Math.random() * 100;
+
 
 
 
@@ -26,6 +28,7 @@ namespace L08_GoldenerHerbst {
         drawCloud();
         drawMountains(startPointofFirstMountainX, horizone);
         //drawTree(0, horizone - 50);
+        drawTree2(randomFirstTreeX, horizone + 50);
         //drawField(0, horizone + 150);
     }
 
@@ -66,6 +69,11 @@ namespace L08_GoldenerHerbst {
             let translateX: number = Math.random() * (-4.5) + (-3.5);
             let translateY: number = Math.random() * (-0.2);
 
+            console.log(translateX);
+            console.log(translateY);
+
+
+
 
 
             clouds.arc(numbX, numbY, 15, 0, 2 * Math.PI);
@@ -85,52 +93,47 @@ namespace L08_GoldenerHerbst {
     function drawMountains(_startPointFirstMountainX: number, _horizoneY: number): void {
 
         //Gradient für die Berge
-        let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, _horizoneY);
-        gradient.addColorStop(0, "lightgrey");
-        gradient.addColorStop(1, "black");
+        let gradient: CanvasGradient = crc2.createLinearGradient(0, 150, 0, _horizoneY);
+        gradient.addColorStop(1, "grey");
+        gradient.addColorStop(0, "white");
 
         let x: number = 0;
-        
+        let drawLineRandomX: number = 0;
 
 
-        debugger;
+
+        //debugger;
 
 
         crc2.save();
 
         //Beginn der Zeichnung für die Berge
-        crc2.beginPath();
-        crc2.moveTo(_startPointFirstMountainX, _horizoneY);
+
 
         for (let i: number = 8; i > 0; i--) {
 
-            
+
             //Zufällige Zahl für Zeichnung in X und Y Richtung
-            let drawLineRandomX: number = Math.random() * crc2.canvas.width;
-            let drawLineRandomY: number = Math.random() * 50 + 150;
+            drawLineRandomX = Math.random() * 50 + 100;
+            let drawLineRandomY: number = Math.random() * 80 + 50;
 
             x += drawLineRandomX;
 
-            /*do {
-                x += stepMin + Math.random() * (stepMax - stepMin);
-                let y: number = - _min - Math.random() * (_max - _min);
-    
-                crc2.lineTo(x, y);
-    
-            } while (x < crc2.canvas.width);
-            */
+
 
             console.log(drawLineRandomX);
             console.log(drawLineRandomY);
 
+            crc2.beginPath();
+            crc2.moveTo(_startPointFirstMountainX, _horizoneY);
             crc2.strokeStyle = "grey";
-            crc2.lineWidth = 5;
-            crc2.lineTo(drawLineRandomX, drawLineRandomY);
-            crc2.lineTo(drawLineRandomX + 150, _horizoneY);
+            crc2.lineWidth = 2;
+            crc2.lineTo(x - 50, drawLineRandomY);
+            crc2.lineTo(x + 150, _horizoneY);
             crc2.fillStyle = gradient;
             crc2.fill();
-            crc2.stroke();
-            
+
+
         }
         crc2.closePath();
         crc2.restore();
@@ -139,6 +142,7 @@ namespace L08_GoldenerHerbst {
 
 
     function drawTree(_startPositionX: number, _startPositionY: number): void {
+
 
         crc2.save();
 
@@ -150,13 +154,13 @@ namespace L08_GoldenerHerbst {
         crc2.save();
 
         crc2.beginPath();
-        crc2.fillStyle = "green";
-        crc2.strokeStyle = "black";
+        crc2.strokeStyle = "green";
         crc2.moveTo(_startPositionX + 100, _startPositionY);
         crc2.lineTo(-10, 0);
         crc2.lineTo(10, 20);
         crc2.lineTo(5, 20);
-        crc2.fill();
+        crc2.lineWidth = 2;
+        crc2.stroke();
         crc2.closePath();
 
         crc2.restore();
@@ -172,6 +176,105 @@ namespace L08_GoldenerHerbst {
         crc2.fillRect(_startPositionX, _startPositionY, 800, 78);
 
         crc2.restore();
+    }
+
+
+
+    function drawTree2(_startPositionX: number, _startPositionY: number): void {
+
+        let treeTrunkX: number = 25;
+        let treeTrunkY: number = 50;
+        let treeBushX: number = 12.5;
+        let treeBushX2: number = 25;
+        let treeBushX3: number = 37.5;
+        let treeBushX4: number = 50;
+        let treeBushY: number = 50;
+        let treeBushY2: number = 75;
+
+        let xAdd: number = 0;
+        let drawRandomTreeOnX: number;
+
+
+
+
+        crc2.save
+
+        /*
+        crc2.beginPath();
+        crc2.moveTo(_startPositionX, _startPositionY);
+        console.log(_startPositionX, _startPositionY);
+        crc2.lineTo(_startPositionX + treeTrunkX, _startPositionY);
+        crc2.lineTo(_startPositionX + treeTrunkX, _startPositionY - treeTrunkY);
+        crc2.lineTo(_startPositionX, _startPositionY - treeTrunkY);
+        crc2.lineTo(_startPositionX, _startPositionY);
+        crc2.stroke();
+        crc2.fillStyle = "brown";
+        crc2.fill();
+        crc2.closePath();
+
+
+
+        crc2.beginPath();
+        crc2.moveTo(_startPositionX, _startPositionY - treeTrunkY);
+        crc2.lineTo(_startPositionX - treeBushX2, _startPositionY - treeBushY);
+        crc2.lineTo(_startPositionX - treeBushX2 + treeBushX3, _startPositionY - treeBushY - treeBushY2);
+        crc2.lineTo(_startPositionX + treeBushX4, _startPositionY - treeBushY);
+        crc2.lineTo(_startPositionX + treeBushX4 - treeBushX4, _startPositionY - treeBushY);
+        crc2.fillStyle = "darkgreen";
+        crc2.fill();
+        crc2.stroke();
+        */
+        for (let i: number = 0; i < 5; i++) {
+
+            drawRandomTreeOnX = Math.random() * 50 + 70;
+
+            xAdd += drawRandomTreeOnX;
+
+            console.log(drawRandomTreeOnX);
+            console.log(xAdd);
+            
+
+            crc2.beginPath();
+            crc2.moveTo(_startPositionX, _startPositionY);
+            console.log(_startPositionX, _startPositionY);
+            crc2.lineTo(_startPositionX + treeTrunkX, _startPositionY);
+            crc2.lineTo(_startPositionX + treeTrunkX, _startPositionY - treeTrunkY);
+            crc2.lineTo(_startPositionX, _startPositionY - treeTrunkY);
+            crc2.lineTo(_startPositionX, _startPositionY);
+            crc2.stroke();
+            crc2.fillStyle = "brown";
+            crc2.fill();
+            crc2.closePath();
+
+
+
+            crc2.beginPath();
+            crc2.moveTo(_startPositionX, _startPositionY - treeTrunkY);
+            crc2.lineTo(_startPositionX - treeBushX2, _startPositionY - treeBushY);
+            crc2.lineTo(_startPositionX - treeBushX2 + treeBushX3, _startPositionY - treeBushY - treeBushY2);
+            crc2.lineTo(_startPositionX + treeBushX4, _startPositionY - treeBushY);
+            crc2.lineTo(_startPositionX + treeBushX4 - treeBushX4, _startPositionY - treeBushY);
+            crc2.fillStyle = "darkgreen";
+            crc2.fill();
+            crc2.stroke();
+
+
+        }
+
+
+        /*
+        crc2.lineTo(_startPositionX + treeTrunkX, _startPositionY - 50);
+        crc2.lineTo(_startPositionX + treeTrunkX + treeTrunkX, _startPositionY - 50);
+        crc2.lineTo(_startPositionX + treeTrunkX - treeBushX , _startPositionY - 50 - treeBushY);
+        //Hälfte des Baumes fertig
+        crc2.lineTo(_startPositionX + treeTrunkX - treeBushX2 , _startPositionY - 50);
+        crc2.stroke();
+        */
+        crc2.restore;
+
+
+
+
     }
 
 
